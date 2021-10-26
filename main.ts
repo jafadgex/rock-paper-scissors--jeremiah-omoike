3bd5f2006d704257ac464827fc863f11b0ecc80c
jafadgex <92938346+jafadgex@users.noreply.github.com>
2021-10-26 14:01:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let tool = 0
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (tool == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
